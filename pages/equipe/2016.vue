29b3d4e181c314c03add3bf684bde72f400f4cb4
Etienne Sanguinetti <etienne.sanguinetti@gmail.com>
2018-01-29 13:31:11 +0100

    Danseuses, nouvelles redirections

--- pages/equipe/2016.vue
+++ pages/equipe/2016.vue
@@ -3,7 +3,6 @@
     <pane type="pane--media" :bgImage="presentation.bgImage" :image="presentation.image" :text="presentation.text"></pane>
     <div class="section">
       <div class="section__description">
-        <!-- <img src="/logos/poles/2017/bureau.png" alt=""> -->
         <h2>Bureau</h2>
       </div>
       <div class="section__membres">
@@ -16,7 +15,6 @@
     </div>
     <div class="section">
       <div class="section__description">
-        <!-- <img src="/logos/poles/2017/communication.png" alt=""> -->
         <h2>Communication</h2>
       </div>
       <div class="section__membres">
@@ -29,7 +27,6 @@
     </div>
     <div class="section">
       <div class="section__description">
-        <!-- <img src="/logos/poles/2017/amour.png" alt=""> -->
         <h2>Amour</h2>
       </div>
       <div class="section__membres">
@@ -42,7 +39,6 @@
     </div>
     <div class="section">
       <div class="section__description">
-        <!-- <img src="/logos/poles/2017/scenario.png" alt=""> -->
         <h2>Scénario</h2>
       </div>
       <div class="section__membres">
@@ -55,12 +51,11 @@
     </div>
     <div class="section">
       <div class="section__description">
-        <img src="/logos/poles/2017/mes.png" alt="">
         <h2>Mise en scène</h2>
       </div>
       <div class="section__membres">
         <div class="trombi_membre" v-for="(membre, idMembre) in equipe.mes" :key="idMembre">
-          <img v-if="membre.photo != ''" :src="'/images/equipes/2017/' + membre.photo" alt="">
+          <img v-if="membre.photo != ''" :src="'/images/equipes/2016/' + membre.photo" alt="">
           <p><b>{{ membre.nom }}</b><br/>
           <i>{{ membre.role }}</i></p>
         </div>
@@ -68,12 +63,11 @@
     </div>
     <div class="section">
       <div class="section__description">
-        <img src="/logos/poles/2017/ac.png" alt="">
         <h2>Acteurs-chanteurs</h2>
       </div>
       <div class="section__membres">
         <div class="trombi_membre" v-for="(membre, idMembre) in equipe.ac" :key="idMembre">
-          <img v-if="membre.photo != ''" :src="'/images/equipes/2017/' + membre.photo" alt="">
+          <img v-if="membre.photo != ''" :src="'/images/equipes/2016/' + membre.photo" alt="">
           <p><b>{{ membre.nom }}</b><br/>
           <i>{{ membre.role }}</i></p>
         </div>
@@ -81,12 +75,11 @@
     </div>
     <div class="section">
       <div class="section__description">
-        <img src="/logos/poles/2017/chant.png" alt="">
         <h2>Chant</h2>
       </div>
       <div class="section__membres">
         <div class="trombi_membre" v-for="(membre, idMembre) in equipe.chant" :key="idMembre">
-          <img v-if="membre.photo != ''" :src="'/images/equipes/2017/' + membre.photo" alt="">
+          <img v-if="membre.photo != ''" :src="'/images/equipes/2016/' + membre.photo" alt="">
           <p><b>{{ membre.nom }}</b><br/>
           <i>{{ membre.role }}</i></p>
         </div>
@@ -94,38 +87,11 @@
     </div>
     <div class="section">
       <div class="section__description">
-        <img src="/logos/poles/2017/composition.png" alt="">
-        <h2>Composition</h2>
-      </div>
-      <div class="section__membres">
-        <div class="trombi_membre" v-for="(membre, idMembre) in equipe.composition" :key="idMembre">
-          <img v-if="membre.photo != ''" :src="'/images/equipes/2017/' + membre.photo" alt="">
-          <p><b>{{ membre.nom }}</b><br/>
-          <i>{{ membre.role }}</i></p>
-        </div>
-      </div>
-    </div>
-    <div class="section">
-      <div class="section__description">
-        <img src="/logos/poles/2017/zikos.png" alt="">
-        <h2>Musique</h2>
-      </div>
-      <div class="section__membres">
-        <div class="trombi_membre" v-for="(membre, idMembre) in equipe.zikos" :key="idMembre">
-          <img v-if="membre.photo != ''" :src="'/images/equipes/2017/' + membre.photo" alt="">
-          <p><b>{{ membre.nom }}</b><br/>
-          <i>{{ membre.role }}</i></p>
-        </div>
-      </div>
-    </div>
-    <div class="section">
-      <div class="section__description">
-        <img src="/logos/poles/2017/danse.png" alt="">
         <h2>Danse</h2>
       </div>
       <div class="section__membres">
         <div class="trombi_membre" v-for="(membre, idMembre) in equipe.danse" :key="idMembre">
-          <img v-if="membre.photo != ''" :src="'/images/equipes/2017/' + membre.photo" alt="">
+          <img v-if="membre.photo != ''" :src="'/images/equipes/2016/' + membre.photo" alt="">
           <p><b>{{ membre.nom }}</b><br/>
           <i>{{ membre.role }}</i></p>
         </div>
@@ -133,12 +99,11 @@
     </div>
     <div class="section">
       <div class="section__description">
-        <img src="/logos/poles/2017/costumes.png" alt="">
-        <h2>Costumes</h2>
+        <h2>Musique</h2>
       </div>
       <div class="section__membres">
-        <div class="trombi_membre" v-for="(membre, idMembre) in equipe.costumes" :key="idMembre">
-          <img v-if="membre.photo != ''" :src="'/images/equipes/2017/' + membre.photo" alt="">
+        <div class="trombi_membre" v-for="(membre, idMembre) in equipe.zikos" :key="idMembre">
+          <img v-if="membre.photo != ''" :src="'/images/equipes/2016/' + membre.photo" alt="">
           <p><b>{{ membre.nom }}</b><br/>
           <i>{{ membre.role }}</i></p>
         </div>
@@ -146,12 +111,11 @@
     </div>
     <div class="section">
       <div class="section__description">
-        <img src="/logos/poles/2017/decors.png" alt="">
-        <h2>Décors</h2>
+        <h2>Composition</h2>
       </div>
       <div class="section__membres">
-        <div class="trombi_membre" v-for="(membre, idMembre) in equipe.decors" :key="idMembre">
-          <img v-if="membre.photo != ''" :src="'/images/equipes/2017/' + membre.photo" alt="">
+        <div class="trombi_membre" v-for="(membre, idMembre) in equipe.composition" :key="idMembre">
+          <img v-if="membre.photo != ''" :src="'/images/equipes/2016/' + membre.photo" alt="">
           <p><b>{{ membre.nom }}</b><br/>
           <i>{{ membre.role }}</i></p>
         </div>
@@ -159,12 +123,35 @@
     </div>
     <div class="section">
       <div class="section__description">
-        <img src="/logos/poles/2017/technique.png" alt="">
         <h2>Technique</h2>
       </div>
       <div class="section__membres">
         <div class="trombi_membre" v-for="(membre, idMembre) in equipe.technique" :key="idMembre">
-          <img v-if="membre.photo != ''" :src="'/images/equipes/2017/' + membre.photo" alt="">
+          <img v-if="membre.photo != ''" :src="'/images/equipes/2016/' + membre.photo" alt="">
+          <p><b>{{ membre.nom }}</b><br/>
+          <i>{{ membre.role }}</i></p>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="section__description">
+        <h2>Décors</h2>
+      </div>
+      <div class="section__membres">
+        <div class="trombi_membre" v-for="(membre, idMembre) in equipe.decors" :key="idMembre">
+          <img v-if="membre.photo != ''" :src="'/images/equipes/2016/' + membre.photo" alt="">
+          <p><b>{{ membre.nom }}</b><br/>
+          <i>{{ membre.role }}</i></p>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="section__description">
+        <h2>Costumes</h2>
+      </div>
+      <div class="section__membres">
+        <div class="trombi_membre" v-for="(membre, idMembre) in equipe.costumes" :key="idMembre">
+          <img v-if="membre.photo != ''" :src="'/images/equipes/2016/' + membre.photo" alt="">
           <p><b>{{ membre.nom }}</b><br/>
           <i>{{ membre.role }}</i></p>
         </div>
@@ -278,6 +265,256 @@ let equipe = {
       role: 'Metteur en scène',
       nom: 'Vincent Breton',
       photo: 'vinbre.png'
+    }
+  ],
+
+  ac: [
+    {
+      role: 'Comtesse Alvarez',
+      nom: 'Amélie Barbier',
+      photo: 'amebar.png'
+    },
+    {
+      role: 'Isabella',
+      nom: 'Ségolène Bouvret',
+      photo: 'segbou.png'
+    },
+    {
+      role: 'Lisa',
+      nom: 'Agathe Deveraux',
+      photo: 'agadev.png'
+    },
+    {
+      role: 'Leandro',
+      nom: 'Colin Maillebuau',
+      photo: 'colmai.png'
+    },
+    {
+      role: 'Duc de la Rioja / Chef aztèque',
+      nom: 'Antoine Rager',
+      photo: 'antrag.png'
+    },
+    {
+      role: 'Monsieur Mousse',
+      nom: 'Pauline Romagon',
+      photo: 'paurom.png'
+    },
+    {
+      role: 'Capitaine Cortes',
+      nom: 'Valérian Seuwin',
+      photo: 'valseu.png'
+    },
+    {
+      role: 'Castillo',
+      nom: 'Maxime Taupiac',
+      photo: 'maxtau.png'
+    }
+  ],
+
+  composition: [
+    {
+      role: 'Compositeur',
+      nom: 'Vivien Geoffroy',
+      photo: 'vivgeo.png'
+    },
+    {
+      role: 'Compositeur',
+      nom: 'Francois-Rémi Mazy',
+      photo: 'framaz.png'
+    },
+    {
+      role: 'Compositeur',
+      nom: 'Marius Blanchard',
+      photo: 'marbla.png'
+    },
+    {
+      role: 'Compositeur',
+      nom: 'Alexandre Cartier',
+      photo: 'alecar.png'
+    },
+    {
+      role: 'Compositeur',
+      nom: 'Théo Delecour',
+      photo: 'thedel.png'
+    }
+  ],
+
+  chant: [
+    {
+      role: 'Respo chant',
+      nom: 'Inès Allard de Grandmaison',
+      photo: 'inedeg.png'
+    },
+    {
+      role: 'Respo chant',
+      nom: 'Gauthier Fanucci',
+      photo: 'gaufan.png'
+    },
+    {
+      role: 'Choriste',
+      nom: 'Justin Brasier de Thuy',
+      photo: 'jusbra.png'
+    },
+    {
+      role: 'Choriste',
+      nom: 'Chloé Thenoz',
+      photo: 'chlthe.png'
+    }
+  ],
+
+  danse: [
+    {
+      role: 'Respo danse',
+      nom: 'Anaïs Brault',
+      photo: 'anabra.png'
+    },
+    {
+      role: 'Respo danse',
+      nom: 'Mathlide Ray',
+      photo: 'matray.png'
+    },
+    {
+      role: 'Danseuse',
+      nom: 'Chama Benabdesselam',
+      photo: 'chaben.png'
+    },
+    {
+      role: 'Danseuse',
+      nom: 'Cyrielle Callerame',
+      photo: 'cyrcal.png'
+    },
+    {
+      role: 'Danseuse',
+      nom: 'Julie Catel',
+      photo: 'julcat.png'
+    },
+    {
+      role: 'Danseur',
+      nom: 'Simon Chevrel',
+      photo: 'simche.png'
+    },
+    {
+      role: 'Danseuse',
+      nom: 'Alice Deroo',
+      photo: 'alider.png'
+    },
+    {
+      role: 'Danseuse',
+      nom: 'Ornella Diascorn',
+      photo: 'orndia.png'
+    },
+    {
+      role: 'Danseuse',
+      nom: 'Anne-Marie Plancke',
+      photo: 'annpla.png'
+    },
+    {
+      role: 'Danseuse',
+      nom: 'Inès Prunières',
+      photo: 'inepru.png'
+    },
+    {
+      role: 'Danseur',
+      nom: 'Thibaut Rahm',
+      photo: 'thirah.png'
+    },
+    {
+      role: 'Danseuse',
+      nom: 'Marianne Sauvanet',
+      photo: 'marsau.png'
+    },
+    {
+      role: 'Danseuse',
+      nom: 'Claire Vignes',
+      photo: 'clavig.png'
+    }
+  ],
+
+  zikos: [
+
+  ],
+
+  technique: [
+    {
+      role: 'VP lumières',
+      nom: 'Paul Duvivier',
+      photo: 'pauduv.png'
+    },
+    {
+      role: 'VP son',
+      nom: 'Donation Delehelle',
+      photo: 'dondel.png'
+    },
+    {
+      role: 'VP son',
+      nom: 'Arthur Lett',
+      photo: 'artlet.png'
+    },
+    {
+      role: 'VP son',
+      nom: 'Farah Ouali',
+      photo: 'faroua.png'
+    },
+    {
+      role: 'VP son',
+      nom: 'Benjamin Thiron',
+      photo: 'benthi.png'
+    },
+    {
+      role: 'VP son',
+      nom: 'Valentin Vie',
+      photo: 'valvie.png'
+    }
+  ],
+
+  decors: [
+    {
+      role: 'Respo décors',
+      nom: 'Julie Hazard',
+      photo: 'julhaz.png'
+    },
+    {
+      role: 'Respo décors',
+      nom: 'Lisa Do Nascimento',
+      photo: 'lisdon.png'
+    },
+    {
+      role: 'Décoratrice',
+      nom: 'Joséphine Larrieu',
+      photo: 'joslar.png'
+    },
+    {
+      role: 'Décoratrice',
+      nom: 'Agathe Poisot',
+      photo: 'agapoi.png'
+    },
+    {
+      role: 'Décoratrice',
+      nom: 'Myriam Billet',
+      photo: 'myrbil.png'
+    }
+  ],
+
+  costumes: [
+    {
+      role: 'Respo costumes',
+      nom: 'Victoire Momméja',
+      photo: 'vicmom.png'
+    },
+    {
+      role: 'Respo costumes',
+      nom: 'Justine Gaudré',
+      photo: 'jusgau.png'
+    },
+    {
+      role: 'Costumière',
+      nom: 'Marianne Coupry',
+      photo: 'marcou.png'
+    },
+    {
+      role: 'Costumière',
+      nom: 'Marie Legrand',
+      photo: 'marleg.png'
     }
   ]
 }
