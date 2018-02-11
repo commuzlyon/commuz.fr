bb06463b01fc431f46a7e9532986bee4f7b9d7a4
Etienne Sanguinetti <etienne.sanguinetti@gmail.com>
2018-02-11 13:06:13 +0100

    Corrections II

--- middleware/redirect_conchiage.js
+++ middleware/redirect_conchiage.js
@@ -1,5 +1,5 @@
 export default function ({ route, redirect }) {
-  let urlConchiage = [ '/jambonbrie', '/jorick', '/rootz' ]
+  let urlConchiage = [ '/jambonbrie', '/jorick', '/rootz', '/commurge' ]
   if (urlConchiage.indexOf(route.fullPath) >= 0) {
     return redirect('/c' + route.fullPath)
   }
