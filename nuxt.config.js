c08a0a2dd150cdc2140cd9105a4b8add418473fa
Etienne Sanguinetti <etienne.sanguinetti@gmail.com>
2018-02-12 20:25:35 +0100

    Faute d'orthographe corrigée

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Commédie musicale étudiante commune à l\'École Centrale de Lyon et à l\'emlyon business school' },
+      { hid: 'description', name: 'description', content: 'Comédie musicale étudiante commune à l\'École Centrale de Lyon et à l\'emlyon business school' },
       { name: 'theme-color', content: '#fe7a90' }
     ],
     link: [
