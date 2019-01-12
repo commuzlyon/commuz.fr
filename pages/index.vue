78c9f87921752a1b5016ae32aa1c4b720f432b09
Raphaël Chalicarne <raphael.chalicarne@outlook.com>
2019-01-12 09:59:44 +0100

    Modification photo (vidéo) page d'accueil
    Lorsque la vidéo ne se charge pas, c'est une copie d'écran de la première image de la vidéo qui s'affiche

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
       <path d="M 14,182 H 276"  stroke="#fff" stroke-width="2px" fill="transparent" style="stroke-linecap: round; stroke-linecap: round; stroke-dashoffset: 524;" />
     </g></svg>
     <a id="home__hero__scroll" onclick="window.scrollTo(0,document.querySelector('#home__hero ~ div').offsetTop)"><img src="/icons/scroll-down.svg" alt="">Découvrir</a>
-    <video id="home__hero_bg" poster="/images/hero_bg_poster.jpg" autoplay loop>
+    <video id="home__hero_bg" poster="/images/video-statique-annees-folles.jpg" autoplay loop>
       <source src="/videos/video_webmaster_15s_webm.webm" type="video/webm">
       <source src="/videos/video_webmaster_15s_mp4.mp4" type="video/mp4">
     </video>
