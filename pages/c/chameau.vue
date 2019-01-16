66855cd8c19e05ed0262d48a766f7fe7f721479c
Raphaël Chalicarne <raphael.chalicarne@outlook.com>
2019-01-17 00:40:54 +0100

    Essai jeu du chameau v1

---
+++ pages/c/chameau.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+<div class="">
+  <a href="t-rex/index.html">Jouer au jeu</a>
+</div>
+</template>
+
+<script>
+
+export default {
+  layout: 'conchiage'
+}
+</script>
+
+<style >
+
+</style>
