75fd41215f17f2cadb6ef6c2b4d42e4a7e1f2a96
Raphaël Chalicarne <raphael.chalicarne@outlook.com>
2019-03-03 12:16:42 +0100

    Redirect conchiage Casting

--- middleware/redirect_conchiage.js
+++ middleware/redirect_conchiage.js
@@ -1,5 +1,5 @@
 export default function ({ route, redirect }) {
-  let urlConchiage = [ '/jambonbrie', '/jorick', '/rootz', '/commurge', '/karla','/casting' ]
+  let urlConchiage = [ '/jambonbrie', '/jorick', '/rootz', '/commurge', '/karla','/casting']
   if (urlConchiage.indexOf(route.fullPath) >= 0) {
     return redirect('/c' + route.fullPath)
   }
