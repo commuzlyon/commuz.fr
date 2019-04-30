ba43239d014a5f5038ddd8e5eeaffd2887ab1d31
XeBasTeX <42714614+XeBasTeX@users.noreply.github.com>
2019-04-30 11:43:20 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'La Coommuz\'',
+    title: 'La Commuz\'',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
