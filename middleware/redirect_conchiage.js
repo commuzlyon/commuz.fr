e8312c839d6fc91e8b62e985bfc44e49be569026
Bastien <bastien.laville@pm.me>
2020-01-11 15:46:00 +0100

    Le conchiage, ça dégage

--- middleware/redirect_conchiage.js
+++ middleware/redirect_conchiage.js
@@ -1,5 +1,5 @@
 export default function ({ route, redirect }) {
-  let urlConchiage = [ '/jambonbrie', '/jorick', '/rootz', '/commurge', '/karla','/casting']
+  let urlConchiage = [ '/commurge','/casting']
   if (urlConchiage.indexOf(route.fullPath) >= 0) {
     return redirect('/c' + route.fullPath)
   }
