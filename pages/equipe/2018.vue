c6f396c4ea24b9bd99c06651570d2c7bdb78a1c3
benjamindenise <benjamin.denise@protonmail.com>
2022-01-08 13:08:44 +0100

    Clean équipe 2018 & 2019

--- pages/equipe/2018.vue
+++ pages/equipe/2018.vue
@@ -100,11 +100,7 @@
       <div class="section__membres">
         <div class="trombi_membre" v-for="(membre, idMembre) in equipe.zikos" :key="idMembre">
           <img v-if="membre.photo != ''" :src="'/images/equipes/2018/' + membre.photo" alt="">
-          <template v-if="membre.nom == 'Marwan Hassan'">
-            <router-link to="/rootz"><p><b>{{ membre.nom }}</b><br/>
-              <i>{{ membre.role }}</i></p></router-link>
-          </template>
-          <template v-else>
+          <template>
             <p><b>{{ membre.nom }}</b><br/>
               <i>{{ membre.role }}</i></p>
           </template>
