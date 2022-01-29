15c565e700c8a72c9efddd878fece5e77f08a21e
benjamindenise <benjamin.denise@protonmail.com>
2022-01-29 18:02:50 +0100

    add commurge

---
+++ pages/c/casting.vue
@@ -0,0 +1,200 @@
+<template lang="html">
+<div>
+    <div class="perles">
+        <h1>{{ perle_text }}</h1>
+        <h3>{{ perle_auteur }}</h3>
+    </div>
+
+    <!--<div class="refresh-button" @click="refresh_perle(n, perles)" >
+        <img src="/c/casting/refresh-button.png">
+    </div>-->
+</div>
+</template>
+
+<script>
+    var perles = [ 	["Le corps est dedans / RIP les paroles","NCV"],
+					["L'avantage c'est que si la vidéo ne marche pas, c'est pas bien grave","NCV"],
+					["Catastrophe, la pire jusqu'à présent","NCV"],
+					["Ça ne sert à rien de s'attarder sur le chant","NCV"],
+					["Chuuuut, parles pas trop fort, j'allais dormir","CB"],
+					["Roux","PA"],
+					["Ils font chier tes cheveux, on voit pas ton visage","PA"],
+					["Il commence par le deuxième couplet, c’est intéressant","IR"],
+					["Elle a des grosses traces de doigt sur ses lunettes maintenant","IR"],
+					["Il faudrait qu'elle chante pour pouvoir juger le timbre","NCV"],
+					["Nein nein nein !","NCV"],
+					["On dirait une sorte de poème iranien prononcé dans une cave du 18ème arrondissement","NCV"],
+					["Elle sent bon","NCV"],
+					["Mou comme les règles Maped qui se tordent","CB"],
+					["Comment il fait pour crier et que ça sonne étouffé","IR"],
+					["Puissance : j’ai l’impression d’entendre mon voisin chanter derrière un mur d’1m20 d’épaisseur","RH"],
+					["GÈRE TOI !!!","CT"],
+					["Timbre : quelconque","NCV"],
+					["Je ne sais pas s’il chante ou parle","JC"],
+					["il me fait rire, il est à fond c'est trop chou on sent qu'il voit un film dans sa tête","AB"],
+					["Tu es faux aujourd'hui, tu piques les oreilles","NCV"],
+					["Le pauvre il capte rien à nos blagues. On lui fait peur.","JC"],
+					["Mâche beaucoup et dit « eum »","JC"],
+					[" « de manière naturelle »","NCV"],
+					["Qu'est-ce que ça peut être long le théâtre quand c'est mauvais","NCV"],
+					["Expérience musicale : j’espère pas","NCV"],
+					["Justesse : non","NCV"],
+					["Il essaie, sans le son on pourrait croire qu'il est à fond","IR"],
+					["(fin de la chanson) Dites moi quand l'intro est finie svp, que je l'entende un peu chanter","CB"],
+					["Je pense qu'il a eu un traumatisme à droite de son corps, je n'ai jamais vu son oreille droite","IR"],
+					["Tellement de constance dans le claquement de doigts","JC"],
+					["Il est trop beau","NCV"],
+					["Bien, mais finit toutes ses phrases en « hhh ». Il a un ton de documentaire animalier c'est génial","IR"],
+					["Il me regarde je pense qu'il m'aime","IR"],
+					["Le truc à la guitare c'est mignon, ça fait youtubeur beauté qui fait une cover","NCV"],
+					["Ça tremble beaucoup, sur les aigus ça fait effet chèvre","NCV"],
+					["Alto coincée peut-être Mezzo si elle se lâche un peu","NCV"],
+					["Rythme : Catastrophe","NCV"],
+					["Y a un rythme ?","PA"],
+					["Note générale : C’est non.","NCV"],
+					["Variété de jeu : la récitation de CM1","NCV"],
+					["Atouts : sa partenaire ressemble à une petite fille dans un film d’horreur","NCV"],
+					["J'aime bien son haut et ses chaussures","NCV"],
+					["Potentiel émotionnel : x","NCV"],
+					["Mise en scène : Bien sur le passage de la tablette","TF"],
+					["Son pote fait genre 2m10. Mais il joue comme un pied","IR"],
+					["Je serais vexé si j’étais l’amour de sa vie","IR"],
+					["Canon : étrangement ça passe","NCV"],
+					["Pas ouf, parfois joli mais ça passe","SR"],
+					["Aussi immobile que ma grand-mère morte","CB"],
+					["« Y a une petite mise en scène » : ah ouais vraiment toute petite !","PA"],
+					["Mal de mer. La fameuse danse de Gilbert Montagné","NCV"],
+					["Le 7e cercle de l'enfer est constitué de diffusion en boucle de scènes de théâtre joués par ces protagonistes","NCV"],
+					["Rien à tirer","GM"],
+					["Très bonne démonstration de lecture","PD"],
+					["Laisse tomber","AD"],
+					["Les euh phrases euh sont euh hachées","NCV"],
+					["Bon gros suceur","JBDT"],
+					["J'aime bien la musique de fond","NCV"],
+					["Potentiel émotionnel : Fait ressentir de la pitié au spectateur quand il demande si quelqu'un peut lui donner la réplique","JBDT"],
+					["Justesse : 18 chansons en une seule, félicitations","JBDT"],
+					["Pousse des drôles de gémissements quand elle oublie ses paroles... je suis troublé","JBDT"],
+					["Elle me met une tête, du coup elle en met deux à Spac","GM"],
+					["Le jour où les spectateurs seront cachés dans des ordinateurs ce sera peut être intéressant.","SB"],
+					["Pfff pas grand chose, elle est mignonne mais c'est tout","SB"],
+					["J'me fais chier","JBDT"],
+					["13 ans de chant : tu parles d'une blague","GM"],
+					["C'est mignon, voire un peu ridiculo-mignon","NCV"],
+					["Note générale : On vous enverra un courrier","AG"],
+					["Manière très élégante de tousser","AG"],
+					["Côté positif : la délibération va être courte","CS"],
+					["Elle a un tatouage de mangemort","YV"],
+					["Avec les cheveux attachés on dirait un peu un poisson","TS"],
+					["-20 points sur Upside pour le faux départ","TS"],
+					["Il a une faible tessiture","YV"],
+					["Interprétation : mains jointes, les yeux au ciel, une religieuse. Se balance dangereusement, j'ai l'impression de voir un sapin dans le vent.","DB"],
+					["Je suis partagé entre sa capacité folle à faire du yaourt en français et son absence de paroles","YV"],
+					["Ne se laisse pas démonter par la non-connaissance des paroles j'aime !!! C'est pro :P","AG"],
+					["Tintin au Pérou, puis Tintin au Congo, puis Tintin et la Castafiore. On a tout eu c'est super !","AG"],
+					["Je vous partage mon état d'esprit en ce début de casting : je veux une alto","AG"],
+					["Elle a choisi une chanson à 4 notes","YV"],
+					["Beaucoup de potentiel. Franchement, il est sexy","CS"],
+					["RRRRRRR","MP"],
+					["Notion très approximative des hauteurs de notes.","AG"],
+					["Elle bouge pas mal mais gigote plus qu'elle ne bouge","TS"],
+					["Vocalises excellentes, tout dans le nez quand elle chante.","DB"],
+					["Balancement de droite à gauche, toujours un rictus serré qui, je pense, nuit à la qualité sonore. ","AG"],
+					["Soprano, j'aime trop","DB"],
+					["Vibrato qui se tranforme en trémolo","YV"],
+					["Un peu poupouffe","NCV"],
+					["Ca a son charme cet auto-tune, mais peut-être pas toutes les 5s.","AG"],
+					["Ça fait très danse de spectacle de camping, mais c'est sympa","NCV"],
+					["Atouts : ses yeux !!!!!! ","DB"],
+					["Il sera bien mieux en danseur","NCV"],
+					["OH PUTAIN ELLE M'A REGARDE JSUIS AMOUREUSE. OU EST MON HETEROSEXUALITE ?","CS"],
+					["Non, son poème en Allemand a des paroles très touchantes mais il n'y met pas de coeur.","DB"],
+					["Très amusant :) timbre sympathique, très gentil.","DB"],
+					["Arrête de marteler le vent avec tes poings.","DB"],
+					["Je suis pas un pro mais je dirais que c'est faux sur le libre.","TS"],
+					["Changement de sexe","MP"],
+					["Casse pas la baraque","BD"],
+					["1 pin, 2 pins… 8 pins, fragile","BD"],
+					["FAUTE DE JEU ON JOUE PAS À L'ENVERS !","IR"],
+					["Il bouge mais ça n'a pas vraiment de sens.","TH"],
+					["Il tangue.","TH"],
+					["Qu'est-ce qu'il fait ? Il incarne bien la selle de cheval par contre","TH"],
+					["Ça va, mais en fait non","VH"],
+					["Mains coincées, mal à l'aise. On prend Sluh plutôt ?","BD"],
+					["Je comprend pas ce qu'il se passe, je crois que ça dépasse mes capacités ici.","TH"],
+					["Occupation de l’espace : bah R","BD"],
+					["Ouvre graaand la bouche !","BD"],
+					["Chant : ANNULASTR","IR"],
+					[" Trop à l'aise, elle s'en bat un peu les couilles","VH"],
+					["Jolie voix, je veux l’épouser","VH"],
+					["Rythme : __________ <- plat","VH"],
+					["Chant en anglais : mixte entre accent français et chinois, c marrant","IR"],
+					["Pas sur de lui du tout, on dirait un papy ? ","VH"],
+					["Pas assez fort, vent force 5","IR"],
+					["On a l'impression qu'il veut faire le fragile quand il chante ","VH"],
+					["Un peu terne ; il parle comme François Mitterand","TH"],
+					["A boycotté l'harmonique","BD"],
+					["English a bit yaourt but sympa","BD"],
+					["Atouts : il fait très bien la goutte, petit « putain » quand il oublie son texte","NC"],
+					["S'est plantée, n’a pas compris que le poste « chanteur » n'existait pas.","VH"],
+					["Potentiel humoristique : aime faire le pitre","VH"],
+					["C’est bien, et en fait non","VH"],
+					["Merci d'être revenu, à l'année prochaine ?","VH"],
+					["Beaucoup de liaisons mal-t-à propos","EB"],
+					["Concentrée, matte plus le plafond que nos trombines","BD"],
+					["Bonne voie de vendeur de tapis","BD"],
+					["Il se croit à l'opéra ??","BD"],
+					["Mauvaise incarnation du tractopelle, on pourra pas lui donner ce genre de rôle","TH"],
+					["C MoR","IR"],
+					["N’a de brillant que le t-shirt","VH"],
+				];
+				
+    var n = perles.length;
+    
+    var refresh_perle = function(n, perles) {
+        var indice = Math.floor((Math.random() * n));
+        var perle_text = perles[indice][0];
+        var perle_auteur = perles[indice][1];
+        var perle_array = [perle_text,perle_auteur];
+        
+        return perle_array;
+    }
+    
+    let perle_array =  refresh_perle(n, perles);
+    let perle_text = perle_array[0];
+    let perle_auteur = perle_array[1];
+    
+export default {
+  layout: 'conchiage',
+  methods: { refresh_perle },
+  data () {
+    return {
+        perle_text,
+        perle_auteur
+    }
+  },
+components: {
+}
+}
+    
+
+</script>
+
+<style lang="scss" scoped>
+    .perles {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 5rem;
+    }
+    
+    .refresh-button {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        
+        img {
+            width: 200px;
+            height: auto;
+        }
+    }
+</style>
---
+++ pages/c/commurge.vue
@@ -0,0 +1,432 @@
+<template lang="html">
+<div style="overflow-y: hidden;">
+  <audio id="commurge_audio" type="audio/mp3" src="/c/commurge/music/jul.mp3" autoplay loop></audio>
+  
+  <div id="commurge__container">
+    <div id="overlay_chope">
+      <img id="love_loader" src="/c/commurge/wink.gif">
+      <div id="match">
+        <div id="match_pictures">
+          <div class="match__person" id="chopeA">
+            <img src="" alt="">
+            <p></p>
+          </div>
+          <div class="match__person" id="chopeB">
+            <img src="" alt="">
+            <p></p>
+          </div>
+        </div>
+        <div id="chopOrNot">
+          <img @click="sendChope('no')" class="answer_chope" id="send_no_chope" src="/c/commurge/puck.png" alt="Puck Emoji">
+          <img @click="sendChope('yes')" class="answer_chope" id="send_chope" src="/c/commurge/lovely.png" alt="Smile Emoji With Earths">
+        </div>
+      </div>
+    </div>
+    <div id="commurge__home">
+      <img class="commurge__image" id="speedy" src="/c/commurge/speedy-gonzales.png" alt="Speedy Gonzales">
+      <img class="commurge__image" id="hands" src="/c/commurge/iloveyou.gif" alt="Hands in Heart">
+      <img class="commurge__image" id="colombe" src="/c/commurge/colombe.gif" alt="Colombe">
+      <button @click="openOverlay()" id="show_chope" type="button" class="commurge__button">Hola ! Te ves sexy !</button>
+    </div>
+    
+  </div>
+</div>
+</template>
+
+<script>
+import anime from 'animejs'
+import ratios from '@/static/c/commurge/ratios.json'
+
+// Si jamais on veut attribuer un token unique par connexion au site
+let hashToken = function () {
+  return(Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15));
+}
+
+let hashUser = hashToken();
+
+let heart
+
+
+// Fait apparaitre les propositions de choppes.
+let openOverlay = function () {
+  let overlayChopeElt = document.getElementById('overlay_chope');
+  let homeElt = document.getElementById('commurge__home');
+
+  // Timeline : voir documentation anime.js
+  let choppeTimeline = anime.timeline();
+
+  choppeTimeline.add({
+    targets: homeElt,
+    opacity : [1, 0],
+    duration : 500,
+    begin : function(anime){
+       genChope();
+    },
+    complete: function(anime) {
+      homeElt.remove();
+    },
+  })
+  choppeTimeline.add({
+    targets: overlayChopeElt,
+    borderRadius: ['50%', '0%'],
+    scale: [0, 1],
+    duration: 2000,
+    easing: 'easeInOutQuart',
+    begin: function() {
+      overlayChopeElt.style.display = 'flex';
+    },
+  })
+
+  // Animation des émojies
+  anime({
+    targets : '.answer_chope',
+    translateY: [-5,5],
+    rotateZ: [-5,5], 
+    autoplay: true,
+    direction: 'alternate',
+    loop: true,
+     easing: 'easeInOutSine'
+  })
+
+}
+
+const noms = ['Le Cerf', 'Julian', 'Yuexuan', 'Etienne', 'Bro', 'Albane', 'Clémence', 'Juliette B', 'Joseph', 'Perrine', 'Océane', 'Félicité', 'Jeanne', 'Camille', 'Raphaël', 'Chloé V', 'Antoine', 
+            'Maëlys', 'Maxime', 'Eva S', 'Justine', 'Manon', 'Solène', 'Lise', 'Lucie', 'Laura', 'Louise', 'Emma D', 'Jean', 'Laurena', 'Mahé', 'Victor', 'Marie F', 
+            'Inès', 'Arthur B', 'Oscar', 'Grégoire', 'Julien D', 'Anatole', 'Marie G', 'Martin', 'Léopold', 'Vadim', 'Tom', 'Chloé P', 'Baptiste', 'Aurélien', 
+            'Thibaut D', 'Boris', 'Carole', 'Théotime', 'Thomas', 'Olivier', 'Cléo', 'Lison', 'Hélène', 'Claire', 'Julien G', 'Daniel', 'Fabien', 'Emma J', 
+            'Adèle', 'Robin', 'Ivain', 'Audrey', 'Flavien', 'Delphine', 'Nicolas', 'Alice', 'Amandine', 'Loïc', 'Arthur C', 'Juliette G', 'Paul', 'Sarah', 'Julie', 'Pauline', 
+            'Jonathan', 'Pia', 'Eloi', 'Alban', 'Eva L', 'Maxence', 'Johan', 'Marie D', 'Emeric', 'Emma L', 'Thibaut C', 'Bérénice', "Valentine"]
+
+// Mis à jours des photos
+let applyChope = function (nom, id) {
+  let chopeElt = document.getElementById(id)
+  let chopeImg = chopeElt.childNodes[0]
+  let chopeP = chopeElt.childNodes[1]
+  chopeImg.src = `/c/commurge/trombi/${nom}.jpg`
+  chopeP.innerHTML = `${nom}`
+}
+
+// Génération d'un couple
+let genChope = function () {
+  
+  // Tirage au sort
+  let chopeA = noms[Math.floor(Math.random() * Math.floor(noms.length))]
+  let chopeB = noms[Math.floor(Math.random() * Math.floor(noms.length))]
+  while (chopeA === chopeB) {
+    chopeB = noms[Math.floor(Math.random() * Math.floor(noms.length))]
+  }
+  // Mis à jour des photos
+  applyChope(chopeA, 'chopeA')
+  applyChope(chopeB, 'chopeB')
+}
+
+// Transistion d'un couple à l'autre 
+let genNouvelleChope = function () {
+
+  let nouvelleChoppeTimeline = anime.timeline();
+  nouvelleChoppeTimeline.add({
+    targets : document.getElementById('overlay_chope'),
+    borderRadius: ['0', '50%'],
+    scale: [1, 0],
+    delay: 1000,
+    duration: 1000,
+    easing: 'easeInOutQuart'
+  });
+  nouvelleChoppeTimeline.add({
+    targets : document.getElementById('overlay_chope'),
+    borderRadius: ['50%', '0%'],
+    scale: [0, 1],
+    duration: 1000,
+    easing: 'easeInOutQuart',
+    begin: function(anime) {
+      genChope()
+    }
+  });
+}
+
+// Envoie réponse positive
+let sendChope = function (answer) {
+  if(['yes', 'no'].includes(answer)) {
+    // Construction de la requête
+    if (answer === "yes") {
+      rainingParticles(["🧡"])
+    }
+    else {
+      rainingParticles(["🤮", "💩"])
+    }
+    let chope = new FormData();
+    chope.append("validay", answer);
+    chope.append('chopeA', document.getElementById('chopeA').childNodes[1].innerHTML);
+    chope.append('chopeB', document.getElementById('chopeB').childNodes[1].innerHTML);
+    chope.append('id', hashUser);
+    chope.append('timestamp', Date.now());
+
+    // Envoie de la requete
+    fetch('https://commuzlyon.alwaysdata.net/vote', {
+          headers: {
+              Accept: 'application/json',
+          },
+          method: 'POST',
+          body: chope,
+        })
+    
+    // Changement de couple
+    genNouvelleChope();
+  }
+  
+}
+
+// Note (BL) : j'ai volontairement laissé tomber le système à base de zapier
+// Il n'était ni pérenne (compte à recréer toutes les 2 semaines) ni efficace (interface GUI brouillonne)
+// À la place, je conseille d'utiliser un système php ultra simple et un compte perso gratuit sur alwaysdata
+// C'est moins élégant, mais on a un contrôle plus fin sur nos données
+
+let rainingParticles = function (particles) {
+    // adaptée de http://xahlee.info/js/js_raining_particles.html
+    
+    const num_of_particles = 50;  // Nombre de particules
+    const driftX = 50;          // Déplacement vertical
+    const gravity = 30;        // Dpalcement horizontal
+    const update_speed = 100;
+ 
+    const ParticleTypes= particles;
+
+    // Dimensions de la page
+    const viewportWidth = document.documentElement.clientWidth;
+    const viewportHeight = document.documentElement.clientHeight;
+
+    const viewSpaceWidth = viewportWidth;
+    const viewSpaceHeight = viewportHeight;
+
+    // Fonction aléatoire
+    const randomInt = ((xmin,xmax) => (Math.floor( Math.random() * (xmax + 1 - xmin) + xmin )));    // Entier aléatoire
+    const randomReal = ((xmin,xmax) => ( Math.random() * (xmax - xmin) + xmin ));                   // Réel aléatoire
+
+    let particle_box = document.getElementById("particle_box");
+    if (!particle_box) {
+      particle_box = document.createElement("div");
+      particle_box.setAttribute("id","particle_box");
+    }
+
+    // Fonction générative d'une particule
+    const f_new_particle = (() => {
+        const yy = document.createElement("div");
+        // Forme de la particule
+        yy. textContent= ParticleTypes[Math.floor( Math.random() * ParticleTypes.length )];
+        // Position X
+        yy["xxleft"]=randomInt(0, viewSpaceWidth);
+        yy.style.left= yy["xxleft"] + "px";
+        // Position Y
+        yy["xxtop"]= -50;
+        yy.style.top= yy["xxtop"] + "px";
+        // Rotation
+        yy["xrotate"] = randomInt(-150, 150);
+        yy.style.transform = "rotate(" + yy["xrotate"] + "deg)";
+        // Taille
+        yy["xsize"]= 10 + randomInt(0,30);
+        yy.style.fontSize = yy["xsize"] + "px";
+
+        yy.style.position="fixed";
+        yy.style.zIndex= randomInt(100,9999) .toString();
+        yy.style.transition= "top linear 1.5s, left linear 1.5s, transform linear 1.5s";
+        return yy;
+    });
+
+    // Création des particules
+    { for (let i=0; i < num_of_particles; i++) { particle_box.appendChild( f_new_particle()) } } ;
+
+    document.body.appendChild(particle_box);
+
+    const particleNodes = Array.from (particle_box.children);
+
+    // Fonction mis à jours de la position
+    const f_update_positions = (() => {
+
+        particleNodes.forEach (((xx) => {
+            xx["xxleft"] +=
+                (() => {
+                    const rnd = Math.random();
+                    if ( rnd < 0.3333 ) {
+                        return 0;
+                    } else if ( rnd < 0.6666) {
+                        return driftX;}
+                    else {
+                        return - driftX;
+                    }
+                }) ();
+
+            xx["xxtop"] = xx["xxtop"] + ( (xx["xsize"]/20) * (gravity) );
+
+            if ( xx["xxtop"] > (viewSpaceHeight)*2 ) {
+                xx.remove();    // Arrivé en bas on supprime la particule
+            } else {
+                xx["xxtop"] = xx["xxtop"] + gravity /10 * randomInt(0,10); };
+
+            if ( xx["xrotate"] !== 0 ) {
+                xx["xrotate"] = xx["xrotate"] + randomInt(-30,60);
+                xx.style.transform = "rotate(" + xx["xrotate"] + "deg)";
+            };
+
+            xx.style.left = xx["xxleft"] + "px";
+            xx.style.top = xx["xxtop"] + "px";
+        }))
+
+        // Si plus de particule on met fin à l'interval
+        if (particleNodes.length == 0){
+            clearInterval(particlesUpdateInterval);
+        }
+
+    });
+    const particlesUpdateInterval = setInterval( f_update_positions , update_speed);
+}
+
+export default {
+  layout: 'conchiage',
+  methods: { openOverlay, sendChope, rainingParticles },
+  mounted: function () {
+    let chansons = ['boom.mp3', 'chimai.mp3', 'dion.mp3', 'feuxamour.mp3', 'jul.mp3', 'queen.mp3'];
+    let i = Math.floor(Math.random() * Math.floor(chansons.length));
+    document.getElementById('commurge_audio').src = `/c/commurge/music/${chansons[i]}`;
+    anime({
+      targets : '#show_chope',
+      autoplay: true,
+      scale: [1,1.1],
+      direction: 'alternate',
+      loop: true,
+      easing: 'easeInOutSine'
+    });
+    anime({
+      targets : '#speedy',
+      autoplay: true,
+      rotateY: [-20,20],
+      direction: 'alternate',
+      loop: true,
+      easing: 'easeInOutSine'
+    })
+  }
+}
+
+</script>
+
+
+<style lang="scss" scoped>
+  @import url('https://fonts.googleapis.com/css?family=Dancing+Script');
+  @import url('https://fonts.googleapis.com/css?family=Pacifico');
+
+  #overlay_chope {
+    display: none;
+    height: 100%;
+    width: 100%;
+    overflow: hidden;
+    align-items: center;
+    justify-content: center;
+  }
+
+  #match{
+    display : flex;
+    flex-direction : column;
+    justify-content: space-around;
+    margin : 0 auto;
+    height: 100%;
+
+    #match_pictures {
+      display : flex;
+      flex-direction : column;
+      text-align: center;
+      @media (min-width: 700px) {
+        flex-direction : row;
+      }
+      .match__person {
+        img {
+          width : 400px;
+          max-width : 70%
+        }
+      }
+    }
+    
+
+    #chopOrNot {
+      display : flex;
+      justify-content : space-around;
+
+      #send_chope {
+      cursor: pointer;
+      height: 70px;
+      top: 30px;
+      left: 50%;
+      }
+
+      #send_no_chope {
+      cursor: pointer;
+      height: 70px;
+      right: 50%;
+      }
+    }
+  }
+  
+  #love_loader {
+  opacity: 0;
+  z-index: 11;
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+  width: 35vw;
+  }
+
+
+  #commurge__container {
+    height: 100vh;
+    width: 100%;
+    align-items: center;
+    justify-items: center;
+    background-color: #480ca8;
+    background-image: url('/c/commurge/stars_bg.gif');
+    background-repeat: repeat;
+
+    #commurge__home {
+      padding : 20px;
+      display : grid;
+      align-items: center;
+      grid-template-columns: repeat(3, 33%);
+      grid-template-rows: repeat(5, 20%);
+      height: 100%;
+      
+      // Bouton "Découvrir une choppe"
+      .commurge__button {
+        grid-area: 4/1/5/4;
+        z-index: 2;
+        background-color: #bb98ff;
+        padding: 1em 2em;
+        border-radius: 50%;
+        font-size: 1.4em;
+        color: white;
+        font-family: 'Dancing Script', cursive;
+        font-weight: bold;
+        max-width : 300px;
+        margin : 0 auto;  
+
+      }
+
+      #speedy {
+        grid-area: 3/2;
+      }
+      #colombe {
+        grid-area: 1/3;
+      }
+      #hands {
+        grid-area: 1/1/3/3;
+      }
+
+      .commurge__image {
+      max-height: 100%;
+      max-width: 100%;
+      margin: 0 auto;
+    }
+
+    }
+    
+
+  }
+
+</style>
