9a42e41ff3d8d83def9ad2afde61e109060ab591
Antoine Haynez <71761263+AntoineHnz@users.noreply.github.com>
2023-02-13 15:20:45 +0000

    Update Commuz2023.vue

--- pages/Commuz2023.vue
+++ pages/Commuz2023.vue
@@ -15,8 +15,8 @@
 
     <!-- Trailer -->
     <div style="text-align:center; margin:2em">
-    <h1>Trailer de l'édition 2023 !</h1> 
-    <iframe id="youtube" width="560" height="315" src="https://www.youtube-nocookie.com/embed/P2c3stcFeZo" title="youtubevideo player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <h1>Trailer de l'édition 2023 !</h1> 
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/P2c3stcFeZo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
     <!-- Presentation des personnages -->
     <div style="text-align:center; margin:2em">
