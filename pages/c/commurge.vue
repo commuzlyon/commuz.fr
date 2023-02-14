67f445a1f52b8fddbc3cb1ebda06a709a26bedca
Antoine Haynez <71761263+AntoineHnz@users.noreply.github.com>
2023-02-14 17:16:50 +0000

    Event St Valentin

--- pages/c/commurge.vue
+++ pages/c/commurge.vue
@@ -26,7 +26,7 @@
         </div>
         <div id="chopOrNot">
           <p @click="sendChope('no')" class="answer_chope" id="send_no_chope">🤮</p>
-          <p @click="sendChope('yes')" class="answer_chope" id="send_chope">🥰</p>
+          <p @click="sendChope('yes'); sendChope('yes')" class="answer_chope" id="send_chope">🥰</p>
         </div>
         <div id="chopOrNot">
           <p> <p id="counter_no" style="color: #00FF00">0</p> / <p id="counter_yes" style="color: Fuchsia">0</p> </p>
