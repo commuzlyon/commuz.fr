28a4e846657bbc87fdaa16a080767df3c9147bc3
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-09-12 18:35:48 +0200

    Add components: AnimatedLink, Carousel, HeroCarousel

---
+++ components/Hero.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :class="'flex gap-4 items-center justify-center ' +
+    (reversed ? ' md:flex-row-reverse' : 'md:flex-row') +
+    ' flex-col'
+    ">
+    <slot />
+
+    <span class="w-4/6 md:w-1/2 caviar text-center p-4">
+      <span class="flex flex-col">
+        <slot name="title" />
+        <slot name="content" />
+      </span>
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  reversed?: boolean;
+}>();
+</script>
---
+++ components/HeroCarousel.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <Hero :reversed="reversed">
+    <Carousel :images="images" class="w-4/6" />
+    <template #title>
+      <span class="font-extrabold text-commuz-500 text-xl">{{ title }}</span>
+    </template>
+    <template #content>
+
+      <span>
+        <slot />
+      </span>
+    </template>
+  </Hero>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  images: Array<string>;
+  reversed?: boolean;
+}>();
+</script>
