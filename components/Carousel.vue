53b20e18c6d9f99f55575aadddaadb8303278fb2
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-10-01 22:53:49 +0200

    Remove fit inside from the Carousel

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -1,6 +1,6 @@
 <template>
   <UCarousel v-slot="{ item }" :items="images" :ui="{ item: 'basis-full' }" class="rounded-lg overflow-hidden" arrows>
-    <NuxtImg class="w-full" draggable="false" :src="item" fit="inside"/>
+    <NuxtImg class="w-full" draggable="false" :src="item"/>
 
     <!-- We add these two icons, to force import them and allow UCarousel to show them -->
     <UIcon name="i-heroicons-chevron-left-20-solid" class="hidden" />
