6dd064d452d7d91d1934589da5f67e95dcb187dd
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-10-01 21:45:22 +0200

    Casting card in links

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,13 @@ export default <Partial<Config>>{
   mode: "jit",
   theme: {
     extend: {
+      backgroundSize: {
+        "size-200": "200% 200%",
+      },
+      backgroundPosition: {
+        "pos-0": "0% 0%",
+        "pos-100": "100% 100%",
+      },
       colors: {
         commuz: {
           50: "#fff0f3",
