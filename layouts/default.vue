4f8f073c5121ad5312b86c333934648391b638be
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-10-01 22:20:05 +0200

    Casting variable in navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,11 +5,3 @@
     <Foot />
   </div>
 </template>
-
-<script setup lang="ts">
-/*
-Set `castingOpen` to true to enable the casting page link in the navigation bar. It will also show a card "La Commuz' recrute" on the main page
-*/
-const castingOpen = useState('castingOpen')
-castingOpen.value = true
-</script>
